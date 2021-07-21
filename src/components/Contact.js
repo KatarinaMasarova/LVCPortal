0f7f4f8fde43f40d9420acac8707ceaac8502503
Your Name <masarova.katar@gmail.com>
2021-07-21 17:08:38 +0200

    formulare

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,70 @@
 import React from 'react';
  
 class Contact extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  }
+
+  inputHandler = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    // if (name === "name") {
+    //   if (!value) {
+    //     alert("Name should not be empty");
+    //   }
+    // }
+    this.setState({[name]: value});
+  }
+
+  // submitHandler = e => {                    // toto nefunguje tak ako by malo
+  //   if(this.state.name === '') {
+  //     alert('Name should not be empty.');
+  //     return false;
+  //   }
+  // }
+
   render() {
     return (
-      <div>
-        <p>Contact Us</p>
+      <div className="main">
+        <div className="container-fluid">
+
+          <div className="row justify-content-center">
+            <div className="form-wrapper col-12 col-sm-9 col-md-7 col-lg-4">
+              <h3 className="text-center mb-3">Contact Us</h3>
+              <form onSubmit={this.submitHandler}>
+                  <div className="mb-3">
+                    <label for="contact_name" className="form-label">Name *</label>
+                    <input type="text" className="form-control" id="contact_name" required name="name" onChange={this.inputHandler}/>
+                  </div>
+                  <div className="mb-3">
+                    <label for="contact_email" className="form-label">Email *</label>
+                    <input type="email" className="form-control" id="contact_email" name="email" required onChange={this.inputHandler}/>
+                  </div>
+                  <div className="mb-3">
+                    <label for="contact_phone" className="form-label">Mobile number *</label>
+                    <input type="text" className="form-control" id="contact_phone" name="phone" required onChange={this.inputHandler}/>
+                  </div>
+                  <div className="mb-3">
+                    <label for="contact_designation" className="form-label">Designation</label>
+                    <input type="text" className="form-control" id="contact_designation" name="designation" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="contact_location" className="form-label">Location</label>
+                    <input type="text" className="form-control" id="contact_location" name="location" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="contact_remarks" className="form-label">Remarks</label>
+                    <textarea className="form-control" id="contact_remarks" name="remarks" />
+                  </div>
+                  <div className="ta-center">
+                  <button type="submit" className="btn btn-success">Submit</button></div>
+                </form>
+            </div>
+          </div>
+
+        </div>
       </div>
     )
   }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,11 +2,59 @@ import React from 'react';
  
 class Registration extends React.Component {
   render() {
-    const { params } = this.props.match
     return (
-      <div>
-        <h1>Registration</h1>
-        <p>{params.id}</p>
+      <div className="main">
+        <div className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="form-wrapper col-12 col-sm-9 col-md-7 col-lg-4">
+              <h3 className="text-center mb-3">Registration Form</h3>
+              <form>
+                  <div className="mb-3">
+                    <label for="reg_name" className="form-label">Name *</label>
+                    <input type="text" className="form-control" id="reg_name" required/>
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_email" className="form-label">Email *</label>
+                    <input type="email" className="form-control" id="reg_email" required />
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_phone" className="form-label">Mobile number *</label>
+                    <input type="text" className="form-control" id="reg_phone" required/>
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_designation" className="form-label">Designation</label>
+                    <input type="text" className="form-control" id="reg_designation" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_location" className="form-label">Location</label>
+                    <input type="text" className="form-control" id="reg_location" />
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_course" className="form-label">Course *</label>
+                    <select class="form-select" id="reg_course" required>
+                      <option value=''>Select a course</option>
+                      <option value="1">MEAN</option>
+                      <option value="2">MERN</option>
+                      <option value="3">Java</option>
+                      <option value="4">DevOps</option>
+                    </select>
+                  </div>
+                  <div className="mb-3">
+                    <label for="reg_remarks" className="form-label">Remarks</label>
+                    <textarea className="form-control" id="reg_remarks" />
+                  </div>
+                  {/* <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div> */}
+                  {/* <label for="inputPassword5" className="form-label">Password</label>
+                  <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" /> */}
+                  <div className="ta-center">
+                  <button type="submit" className="btn btn-success">Submit</button></div>
+                </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
